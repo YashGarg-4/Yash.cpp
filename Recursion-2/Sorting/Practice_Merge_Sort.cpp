16d6feb4b5d55d2bb7887f544dcf1edf572597dc
YashGarg-4 <yashgarg04@gmail.com>
2024-08-29 19:39:21 +0530

    29th Aug '24

--- Recursion-2/Sorting/Practice_Merge_Sort.cpp
+++ Recursion-2/Sorting/Practice_Merge_Sort.cpp
@@ -1,7 +1,53 @@
 #include <iostream>
 using namespace std;
 
-void merge(){
+void merge(int a[], int s, int mid, int e){
+
+    int n1 = mid - s + 1;
+    int n2 = e - mid;
+
+    int temp1[n1] = {0};
+    int temp2[n2] = {0};
+
+    for (int i = 0; i < n1 ; i++){
+        temp1[i] = a[s + i]; 
+    }
+
+    for (int i = 0; i < n2 ; i++){
+        temp2[i] = a[mid + 1 + i]; 
+    }
+
+    int i = 0, j= 0, k = s;
+    
+    while (i < n1 && j < n2){
+        if (temp1[i] < temp2[j]){
+            a[k] = temp1[i];
+            k++;
+            i++;
+        } else {
+            a[k] = temp2[j];
+            k++;
+            j++;
+        }
+    }
+
+    while (i<n1){
+        a[k] = temp1[i];
+        i++;
+        k++;
+    }
+
+    while (i<n1){
+        a[k] = temp1[i];
+        i++;
+        k++;
+    }
+
+    while (j<n2){
+        a[k] = temp2[j];
+        j++;
+        k++;
+    }
 
 
 
@@ -18,12 +64,19 @@ void merge_sort(int a[], int s, int e){
 
     merge_sort(a, s, mid);
     merge_sort(a, mid + 1, e);
-    merge(a, s, mid, e)
+    merge(a, s, mid, e);
 
 }
 
 int main(){
 
+    int a[5] = {5,4,3,2,1};
+    
+    merge_sort(a, 0, 4);
 
+    for (int i =0; i < 5; i++){
+        cout << a[i] << ' ';
+    }
 
+    return 0;
 }
